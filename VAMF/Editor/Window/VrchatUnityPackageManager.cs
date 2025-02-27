62ece196bb1ab72673137244327309a2408938ba
4OF <404.fof.nf@gmail.com>
2025-02-27 12:34:14 +0900

    fix: Reset selectedSupportAvatar

--- VAMF/Editor/Window/VrchatUnityPackageManager.cs
+++ VAMF/Editor/Window/VrchatUnityPackageManager.cs
@@ -190,6 +190,7 @@ namespace Window {
                     if(GUILayout.Button("Reset", Style.searchButton)) {
                         searchName = "";
                         searchDescription = "";
+                        selectedSupportAvatar = "None";
                         filteredAssetList = assetDataList;
                         GUI.FocusControl(null);
                     }
